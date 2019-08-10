e81581f751993411fbd8c221e5932f7d00783dd3
Rajesh <rk845280@gmail.com>
2019-08-10 08:56:21 +0530

    backend work done

---
+++ routes/profile.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const fetch = require('node-fetch');
+
+const router = express.Router();
+
+router.get('/:platform/:gamertag', async (req,res) => {
+  try {
+    const headers = {
+      'TRN-Api-Key': process.env.TRACKER_API_KEY
+    };
+
+    const {platform, gamertag} = req.params;
+    const response = await fetch(`${process.env.TRACKER_API_URL}/profile/${platform}/${gamertag}`, {
+      headers
+    });
+
+    const data = await response.json();
+
+    //If no profile is found for the entered gamertag return status not found to the user
+    if(data.errors && data.errors.length > 0) {
+      return res.status(404).json({
+        message: 'Profile not found'
+      });
+    }
+    res.json(data);
+  } catch (error) {
+    console.log(err);
+    res.status(500).json({
+      message: 'Servor error'
+    });
+  }
+});
+
+module.exports = router;
+
---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+
+//Load Env
+dotenv.config({path: './config.env'});
+
+const app = express();
+
+//Dev logging
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+//profile routes
+app.use('/api/v1/profile', require('./routes/profile'));
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`);
+});
